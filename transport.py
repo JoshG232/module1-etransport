404e44869f45231427599dee9fcd5db341ae9a15
JoshG232 <62026593+JoshG232@users.noreply.github.com>
2021-10-09 15:53:35 +0100

    first commit

---
+++ transport.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,username,password,personType):
+        self.username = username
+        self.password = password
+        self.personType = personType
+    def login():
+        print("Temp Login")
+    def signUp():
+        print("Temp signUp")
+
+
+person1 = Person("Josh","password123","Cargo Owner")
+
